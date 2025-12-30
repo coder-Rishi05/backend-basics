e48f21943e7b0d5c9d02c38c6fe6ddfce5537a6a
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-12-30 17:31:08 +0530

    added update route

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -52,13 +52,28 @@ const getAllpost = async (req, res) => {
 
 // update post
 
-const updatePost = async () => {
+const updatePost = async (req,res) => {
   //   const { name, description, age } = req.body;
   // basic validation
-  if (Object.keys(req.body).length === 0)
-    return res.status(400).json({
-      message: "No data provided !",
+  try {
+    if (Object.keys(req.body).length === 0)
+      return res.status(400).json({
+        message: "No data provided !",
+      });
+
+    const post = await Post.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
     });
+
+    if (!post) return res.status(404).json({ message: "post not found !" });
+
+    return res.status(201).json({ message: "Post updated sucessfully", post });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Server Error",
+      error,
+    });
+  }
 };
 
-export { createPost, getAllpost };
+export { createPost, getAllpost, updatePost };
--- backend/src/routes/post.route.js
+++ backend/src/routes/post.route.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 
-import { createPost, getAllpost } from "../controllers/post.controller.js";
+import { createPost, getAllpost, updatePost } from "../controllers/post.controller.js";
 
 const router = Router();
 
 router.route("/create").post(createPost);
 
 router.route("/get").get(getAllpost);
+
+router.route("/update/:id").post(updatePost);
 
 export default router;
